94c9094874412f3bcd52801058d99f00b872fc39
Sudha <sudha_skb@yahoo.com>
2021-06-09 00:15:38 +0800

    QuestionsEmployee

---
+++ QuestionsEmployee/QuestionsEmployee/EmployeePromotion.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestionsEmployee
+{
+    public class Employeepromotion
+    {
+        List<string> employees = new List<string>();
+        public void employeepromotion()
+        {
+            Console.WriteLine("Please enter the promotion list");
+
+            string promoname = "no";
+
+            do
+            {
+                // Employee emp = new Employee();
+                promoname = Console.ReadLine();
+                employees.Add(promoname);
+                //Console.WriteLine("Do you like to continue?? If not then enter no");
+                // string str = Console.ReadLine().ToLower();
+            } while (promoname.ToLower() != "no");
+            Console.WriteLine("EMPLOYEES LIST INTHE ORDER OF PROMOTION");
+            for (int i = 0; i < employees.Count - 1; i++)
+            {
+                Console.WriteLine(employees[i]);
+            }
+        }
+        public void findindex()
+        {
+            Console.WriteLine("Please enter the employee name do you want to find the position");
+            string ename = Console.ReadLine();
+            for (int i = 0; i < employees.Count; i++)
+            {
+                if (employees[i] == ename)
+                {
+                    Console.WriteLine($"The employee is at {i+1} position ");
+                }
+            }
+        }
+        public void sort()
+        {
+            employees.Sort();
+            Console.WriteLine("EMPLOYEES IN ASCENDING ORDER");
+            foreach (var item in employees)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        
+
+    }
+}
+
---
+++ QuestionsEmployee/QuestionsEmployee/PrintEmployee.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestionsEmployee
+{
+    class PrintEmployee
+    {
+        Dictionary <int,Employee> employees = new Dictionary<int,Employee >();
+        string choice="no";
+        /// <summary>
+        /// add employee details and store in a dictionary
+        /// </summary>
+
+        public void AddEmployee()
+        {
+            do
+            {
+                Employee emp = new Employee();
+                emp.TakeEmployeeDetailsFromUser();
+                employees.Add(emp.Id, emp);
+                Console.WriteLine("Do you want to continue yes/no");
+                choice = Console.ReadLine().ToLower();
+            } while( choice!= "no");
+            foreach (var item in employees.Values)
+            {
+                Console.WriteLine(item);
+            }
+
+
+
+        }
+
+        
+
+        public void GetEmployeeById()
+        {
+            Console.WriteLine("Please provide the employee id ");
+            int id = Convert.ToInt32(Console.ReadLine());
+            foreach (var item in employees.Values)
+                {
+                if (id==item.Id)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+        public void sort()
+        {
+            List<Employee> emplist= new List<Employee>();
+            emplist = employees.Values.ToList();
+            emplist.Sort((e1, e2) => e1.Salary.CompareTo(e2.Salary));
+          // emplist.Sort();
+            foreach(var item in emplist)
+            {
+                Console.WriteLine(item);
+            }
+
+            
+           
+
+        }
+        
+
+
+
+    }
+}
