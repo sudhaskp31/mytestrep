73092718da8b9e712ee4a16c865c8ce6418f9b95
Sudha <sudha_skb@yahoo.com>
2021-06-09 00:20:29 +0800

    Account

---
+++ Account/Account/IBank.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Account
+{
+    interface IBank
+    {
+        public void ApplyFine();
+        public void RetrieveCardAmount();
+        public void BarAccount();
+    }
+}
---
+++ Account/Account/IUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Account
+{
+    interface IUser
+    {
+        public void OpenAccount();
+
+        public void DoTransaction();
+
+        public void CloseAccount();
+        
+    }
+}
